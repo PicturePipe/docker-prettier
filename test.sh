543f7207b03a43b61281168cf4cd7465ab0af5f0
Markus Zapke-Gründemann <markus.zapke@picturepipe.com>
2019-03-04 15:33:32 +0100

    Convert tabs to spaces

--- test.sh
+++ test.sh
@@ -7,27 +7,27 @@ set -o pipefail
 cd "$(dirname "$0")"
 
 if [ -z "${CI:-}" ]; then
-	DOCKER_IMAGE_SPEC="prettier"
+    DOCKER_IMAGE_SPEC="prettier"
 else
-	# When we are running on CircleCI, retrieve the image and tag which we just built.
-	# This is a bit hacky, but we don't have access to the parameters passed to docker-publish/build.
-	DOCKER_IMAGE_SPEC="$(docker images --format "{{.Repository}}:{{.Tag}}" | grep "${DOCKER_IMAGE}:")"
+    # When we are running on CircleCI, retrieve the image and tag which we just built.
+    # This is a bit hacky, but we don't have access to the parameters passed to docker-publish/build.
+    DOCKER_IMAGE_SPEC="$(docker images --format "{{.Repository}}:{{.Tag}}" | grep "${DOCKER_IMAGE}:")"
 fi
 
 echo "==> Checking if prettier is present in the image..." >&2
 if docker run --rm "$DOCKER_IMAGE_SPEC" prettier -v > /dev/null; then
-	echo "==> prettier found." >&2
+    echo "==> prettier found." >&2
 else
-	echo "==> prettier seems to be broken!" >&2
-	exit 1
+    echo "==> prettier seems to be broken!" >&2
+    exit 1
 fi
 
 echo "==> Checking if git is present in the image..." >&2
 if docker run --rm "$DOCKER_IMAGE_SPEC" git version | grep -q 'git version'; then
-	echo "==> Git found." >&2
+    echo "==> Git found." >&2
 else
-	echo "==> Git seems to be broken!" >&2
-	exit 1
+    echo "==> Git seems to be broken!" >&2
+    exit 1
 fi
 
 docker volume rm prettier-test >/dev/null 2>&1 || true
@@ -37,10 +37,10 @@ tar c tests | "${DOCKER_CMD[@]}" tar x
 
 echo "==> Checking if prettier complains about broken files as expected..." >&2
 if "${DOCKER_CMD[@]}" prettier --check 'tests/**'; then
-	echo "==> Prettier did not complain as expected!" >&2
-	exit 1
+    echo "==> Prettier did not complain as expected!" >&2
+    exit 1
 else
-	echo "==> Prettier complained as expected." >&2
+    echo "==> Prettier complained as expected." >&2
 fi
 
 # We do not want to expand the following backticks
@@ -50,8 +50,8 @@ echo '==> Cleaning up files with `prettier --write`...' >&2
 
 echo "==> Checking if prettier has fixed the files..." >&2
 if "${DOCKER_CMD[@]}" prettier --check 'tests/**'; then
-	echo "==> Prettier seems to work alright." >&2
+    echo "==> Prettier seems to work alright." >&2
 else
-	echo "==> Prettier write did not behave as expected!" >&2
-	exit 1
+    echo "==> Prettier write did not behave as expected!" >&2
+    exit 1
 fi
